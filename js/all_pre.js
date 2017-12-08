1e6bb1202960902b8d4ffec31460fc146025013a
Glen Husman <wappokickingdeer@gmail.com>
2017-12-07 22:16:24 -0800

    Revert "Setting for blue theme now has effect" Has a noticeable performance hit
    This reverts commit 88d19258c8a6e9331fcc03e28fae285988652dbe.

--- js/all_pre.js
+++
@@ -1,5 +0,0 @@
-browser.storage.local.get("bluetheme").then(function (result) {
-    if (result.bluetheme) {
-        browser.runtime.sendMessage({ injectBlue: true }, function (response) {});
-    }
-});
--- js/settingsmessenger.js
+++
@@ -1,9 +0,0 @@
-// background script
-browser.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.injectBlue) {
-        browser.tabs.insertCSS(sender.tab.id, { file: "/css/all.css" }, function () {
-            sendResponse({ done: true });
-        });
-        return true; // Required for async sendResponse()
-    }
-});
