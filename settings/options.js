0b792f74623a8e8741cad9ef6b18c4ff7961d74a
Glen Husman <wappokickingdeer@gmail.com>
2017-12-07 22:21:39 -0800

    Removed settings UI for blue theme

--- settings/options.js
+++ settings/options.js
@@ -1,18 +1,11 @@
 function saveOptions(e) {
     e.preventDefault();
     browser.storage.local.set({
-        bluetheme: document.getElementById("bluetheme-enable").checked,
         rosterdl: document.getElementById("rosterdl-enable").checked
     });
 }
 
 function restoreOptions() {
-
-    function setBlueTheme(result) {
-        let bluethemeEnable = document.getElementById("bluetheme-enable");
-        bluethemeEnable.checked = result.bluetheme === null ? true : result.bluetheme;
-        document.getElementById("bluetheme-disable").checked = !bluethemeEnable.checked;
-    }
 
     function setRosterDl(result) {
         let rosterdlEnable = document.getElementById("rosterdl-enable");
@@ -24,7 +17,6 @@ function restoreOptions() {
         console.log(`Error retrieving preference: ${error}`);
     }
 
-    browser.storage.local.get("bluetheme").then(setBlueTheme, onError);
     browser.storage.local.get("rosterdl").then(setRosterDl, onError);
 }
 
